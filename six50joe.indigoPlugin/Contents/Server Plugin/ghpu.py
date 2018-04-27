bfa79d49275ae837d0329a73974d5ed6749dfa42
Indigo <indigo@Home-Imac.local>
2018-04-27 06:50:13 -0400

    Trying fix for ghpu _GET

--- six50joe.indigoPlugin/Contents/Server Plugin/ghpu.py
+++ six50joe.indigoPlugin/Contents/Server Plugin/ghpu.py
@@ -127,34 +127,34 @@ class GitHubPluginUpdater(object):
 
         return (limit, remain, resetAt)
 
-    #---------------------------------------------------------------------------
-    # form a GET request to api.github.com and return the parsed JSON response
-    def _GET(self, requestPath):
-        self._debug('GET %s' % requestPath)
+   def _GET(self, requestPath):
+      self.logger.debug('GET %s' % requestPath)
+      headers = {
+         'User-Agent': 'Indigo-Plugin-Updater',
+         'Accept': 'application/vnd.github.v3+json'
+      }
+      data = None
+      requestPath = 'https://api.github.com'+ requestPath
+      #conn = httplib.HTTPSConnection('api.github.com')
+      #conn.request('GET', requestPath, None, headers)
+      #resp = conn.getresponse()
+      f = subprocess.Popen(["curl",  requestPath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+   #'-H', str(headers), "-k",
+      out, err = f.communicate()
+      self.logger.debug(u'HTTP Err result:'+unicode(err) )
+      self.logger.debug(u'ReturnCode:{0}'.format(unicode(f.returncode)))
+      #self.sleep(1)
+      if (int(f.returncode) == 0):
+         data = json.loads(out)
+         self.logger.debug(u'Json results:'+unicode(data))
+      elif (400 <= f.status < 500):
+         error = json.loads(out)
+         self.logger.error('%s' % error['message'])
+      else:
+         self.logger.error('Error: %s' % unicode(err))
 
-        headers = {
-            'User-Agent': 'Indigo-Plugin-Updater',
-            'Accept': 'application/vnd.github.v3+json'
-        }
-
-        data = None
-
-        conn = httplib.HTTPSConnection('api.github.com')
-        conn.request('GET', requestPath, None, headers)
-
-        resp = conn.getresponse()
-        self._debug('HTTP %d %s' % (resp.status, resp.reason))
-
-        if (resp.status == 200):
-            data = json.loads(resp.read())
-        elif (400 <= resp.status < 500):
-            error = json.loads(resp.read())
-            self._error('%s' % error['message'])
-        else:
-            self._error('Error: %s' % resp.reason)
-
-        return data
-
+      return data
+  
     #---------------------------------------------------------------------------
     # prepare for an update
     def _prepareForUpdate(self, currentVersion=None):
